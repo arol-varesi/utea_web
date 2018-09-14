cc03abab854cd3101bb9f6b6fc4bffdbf0b34f8c
arol-varesi <a.varesi@arol.it>
2018-09-14 17:44:48 +0200

    Aggiunto codice modulo "accounts" (no working)

--- utea_web/utea_web/settings.py
+++ utea_web/utea_web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',     # Homepage and global views
+    'accounts',
     'moduli',
 
 ]
--- utea_web/utea_web/urls.py
+++ utea_web/utea_web/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('utenti/', include('accounts.urls')),
+    path('utenti/', include('django.contrib.auth.urls')),
 ]
