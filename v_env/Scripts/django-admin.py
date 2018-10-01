a734b1b97df2042b1c68a0c6efbc3d26b3f13f35
arol-varesi <a.varesi@arol.it>
2018-10-01 10:36:54 +0200

    Aggiornato Virtual Environment, creato da Windows.

--- v_env/Scripts/django-admin.py
+++ v_env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/amelamela/Developer/django/utea_web_project/v_env/bin/python3
+#!e:\50_sw_pc-web\django\utea_web_project\v_env\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
