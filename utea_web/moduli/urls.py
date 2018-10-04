0ef878a371e70783b8b5789d641825d73f86b6db
arol-varesi <a.varesi@arol.it>
2018-10-04 17:35:03 +0200

    SiglaAddView

--- utea_web/moduli/urls.py
+++ utea_web/moduli/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SigleView
+from .views import SigleView, SiglaAddView
 
 urlpatterns = [
-    path('sigle/', SigleView.as_view(), name="sigle"),
+    path('sigle/', SigleView.as_view(), name='sigle'),
+    path('sigle/new/', SiglaAddView.as_view(), name='sigle_add')
 ]
--- utea_web/moduli/views.py
+++ utea_web/moduli/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Sigla, Tipo_componente, Traduzione
 
 # Create your views here.
 
 class SigleView(ListView):
     model = Sigla
+
+
+class SiglaAddView(CreateView):
+    model = Sigla
+    fields = ['sigla', 'descrizione', 'tipo']
+    success_url = 'sigle'
