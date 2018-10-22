38f7cd3444c6eb1d5a5343bf871f63f4d698d0b8
arol-varesi <a.varesiarol.it>
2018-10-22 22:36:58 +0200

    Diverse prove interfaccia "Sigle"

--- utea_web/moduli/urls.py
+++ utea_web/moduli/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SigleView, SiglaNewView, SiglaEditView, SiglaDeleteView
+from .views import SigleView, Sigle2View, SigleViewEdit, SiglaNewView, SiglaEditView, SiglaDeleteView
 
 app_name = 'moduli'
 urlpatterns = [
@@ -7,4 +7,8 @@ urlpatterns = [
     path('sigle/new/', SiglaNewView.as_view(), name='sigla-new'),
     path('sigle/<int:pk>', SiglaEditView.as_view(), name='sigla-edit'),
     path('sigle/<int:pk>/delete/', SiglaDeleteView.as_view(), name='sigla-delete'),
+    path('sigle2/', Sigle2View.as_view(), name='sigle-test2'),
+    path('sigle_editable/', SigleViewEdit.as_view(), name='sigle-list-edit'),
+
+
 ]
--- utea_web/moduli/views.py
+++ utea_web/moduli/views.py
@@ -3,11 +3,31 @@ from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Sigla, Tipo_componente, Traduzione
 from .forms import SiglaForm
+from core.models import Lingua
 
 # Create your views here.
 
 class SigleView(ListView):
     model = Sigla
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['edit_form'] = SiglaForm
+        return context
+
+
+class Sigle2View(ListView):
+    model = Sigla
+    template_name = "moduli/sigle_test2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lingua'] = Lingua.objects.all()
+        return context
+
+class SigleViewEdit(ListView):
+    model = Sigla
+    template_name = "moduli/sigla_list_edit.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
