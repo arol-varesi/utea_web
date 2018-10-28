6f2894b7b5b2c026a9a72ad0247a539a872d27c1
arol-varesi <a.varesiarol.it>
2018-10-28 23:44:22 +0100

    corretto url Sigla-new

--- utea_web/moduli/urls.py
+++ utea_web/moduli/urls.py
@@ -5,7 +5,7 @@ from moduli import views
 app_name = 'moduli'
 urlpatterns = [
 #    path('sigla/new', views.new_sigla, name='new-sigla'),
-    path('sigla/new', SiglaNewView.as_view(), name='new-sigla'),
+    path('sigla/new', SiglaNewView.as_view(), name='sigla-new'),
     path('sigle/', SigleView.as_view(), name='sigle-list'),
     path('sigle/<int:pk>', SiglaEditView.as_view(), name='sigla-edit'),
     path('sigle/<int:pk>/delete/', SiglaDeleteView.as_view(), name='sigla-delete'),
